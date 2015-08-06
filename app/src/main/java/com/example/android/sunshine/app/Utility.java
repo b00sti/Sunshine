6595e8c7c81a266d6c93d379773e2d978abcf33c
b00sti <pawlik.dominik@gmail.com>
2015-08-06 09:52:33 +0200

    part4c:
    finish4

--- app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -18,8 +18,10 @@ package com.example.android.sunshine.app;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.text.format.Time;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Utility {
@@ -36,18 +38,83 @@ public class Utility {
                 .equals(context.getString(R.string.pref_units_metric));
     }
 
-    static String formatTemperature(double temperature, boolean isMetric) {
+    static String formatTemperature(Context context, double temperature, boolean isMetric) {
         double temp;
         if ( !isMetric ) {
             temp = 9*temperature/5+32;
         } else {
             temp = temperature;
         }
-        return String.format("%.0f", temp);
+        return context.getString(R.string.format_temperature, temp);
     }
 
-    static String formatDate(long dateInMillis) {
-        Date date = new Date(dateInMillis);
+    static String formatDate(long dateInMilliseconds) {
+        Date date = new Date(dateInMilliseconds);
         return DateFormat.getDateInstance().format(date);
     }
+
+    public static final String DATE_FORMAT = "yyyyMMdd";
+
+    public static String getFriendlyDayString(Context context, long dateInMillis)
+    {
+        Time time = new Time();
+        time.setToNow();
+        long currentTime = System.currentTimeMillis();
+        int julianDay = Time.getJulianDay(dateInMillis, time.gmtoff);
+        int currentJulianDay =Time.getJulianDay(currentTime, time.gmtoff);
+
+        if(julianDay == currentJulianDay)
+        {
+            String today = context.getString(R.string.today);
+            int formatId = R.string.format_full_friendly_date;
+            return String.format(context.getString(
+                    formatId,
+                    today,
+                    getFormattedMonthDay(context, dateInMillis)
+            ));
+        }else if ( julianDay < currentJulianDay + 7)
+        {
+            return Character.toString(getDayName(context, dateInMillis).charAt(0)).toUpperCase() +
+                    getDayName(context, dateInMillis).substring(1);
+        }else
+        {
+            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("MMM dd, yyyy");
+            return Character.toString(shortenedDateFormat.format(dateInMillis).charAt(0)).toUpperCase() +
+                    shortenedDateFormat.format(dateInMillis).substring(1);
+        }
+    }
+
+    public static String getDayName(Context context, long dateInMillis)
+    {
+        Time time = new Time();
+        time.setToNow();
+        int julianDay = Time.getJulianDay(dateInMillis, time.gmtoff);
+        int currentJulianDay =Time.getJulianDay(System.currentTimeMillis(), time.gmtoff);
+
+        if (julianDay == currentJulianDay)
+        {
+            return context.getString(R.string.today);
+        }else if(julianDay == currentJulianDay + 1)
+        {
+            return context.getString(R.string.tomorrow);
+        }else
+        {
+            Time t = new Time();
+            time.setToNow();
+            SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+            return dayFormat.format(dateInMillis);
+        }
+    }
+
+    public static String getFormattedMonthDay(Context context, long dateInMillis)
+    {
+        Time time = new Time();
+        SimpleDateFormat dbDateFormat = new SimpleDateFormat(Utility.DATE_FORMAT);
+        SimpleDateFormat monthDayFormat = new SimpleDateFormat("MMM dd");
+        String monthDayString = monthDayFormat.format(dateInMillis);
+        return Character.toString(monthDayString.charAt(0)).toUpperCase() +
+                monthDayString.substring(1);
+    }
+
+
 }
